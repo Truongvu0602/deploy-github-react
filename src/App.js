b0b9261a411a0f1b0a39fbf823ade1d7a0621aae
Truong Vu <truongvu0602@example.com>
2024-10-22 20:41:39 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Browse from "./pages/browse/Browse";
 import Search from "./pages/search/Search";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
